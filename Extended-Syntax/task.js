d0391837b2a01e67460776f321a8d4810afb83f4
ivankukarin <ikukarin@yandex.ru>
2019-06-27 16:18:18 +0400

    task3 is done

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -29,6 +29,8 @@ function getResult(a,b,c) {
  
 }
 
+
+
 function calculateDrinkTask(){
     let name = window.personName.value;
     let dateOfBirthday = new Date(window.dateOfBirthday.value);
@@ -37,10 +39,26 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
+    let now = new Date();
+    let resultOfDrink;
+
+    // console.log((now.getFullYear() - dateOfBirthday.getFullYear() ) > 18 );  // для проверки условий по отдельности
+   
+    // console.log(( (now.getFullYear() - dateOfBirthday.getFullYear() ) === 18 ) && ( (now.getMonth() - dateOfBirthday.getMonth() ) >= 1 ));
+
+    // console.log(( (now.getFullYear() - dateOfBirthday.getFullYear() ) === 18 ) && (( now.getMonth() - dateOfBirthday.getMonth() ) === 0 ) && ( ( now.getDate() - dateOfBirthday.getDate()) >= 0 ) ) ;
+
+    if ( ((now.getFullYear() - dateOfBirthday.getFullYear()) > 18 ) || (( (now.getFullYear() - dateOfBirthday.getFullYear() ) === 18 ) && ( (now.getMonth() - dateOfBirthday.getMonth() ) >= 1 ))  || (( (now.getFullYear() - dateOfBirthday.getFullYear() ) === 18 ) && (( now.getMonth() - dateOfBirthday.getMonth() ) === 0 ) && ( ( now.getDate() - dateOfBirthday.getDate()) >= 0 ) )  ) {
+        resultOfDrink = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        resultOfDrink = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }  
+    
+    return resultOfDrink;
+   
 }
+
+
 
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
@@ -60,15 +78,15 @@ function getAverageMark(marks){
 
     let sumOfMarks = 0;
 
-    // for (let i = 0; i < marks.length; i++) {
-    //     console.log(marks[i]);
-    //     sumOfMarks += marks[i];
-    //     console.log(sumOfMarks);
-    // }
+    for (let i = 0; i < marks.length; i++) {  
+        console.log(marks[i]);
+        sumOfMarks += marks[i];
+        console.log(sumOfMarks);
+    }
 
-    for (let mark of marks) {   
-        sumOfMarks += mark
-    };
+    // for (let mark of marks) {   //работает оба варианта перебора массива
+    //     sumOfMarks += mark
+    // };
 
     let averageMark = (sumOfMarks / marks.length);
     console.log(averageMark);
