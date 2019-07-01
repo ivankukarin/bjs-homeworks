ebec6b15318f03154caf5eeb73eb054080fbe049
ivankukarin <ikukarin@yandex.ru>
2019-07-01 14:04:58 +0400

    task3 is done,but i have doubt that task 3 solution is "best practices"

--- functions/main.js
+++ functions/main.js
@@ -1,101 +1,101 @@
-// 'use strict'
+'use strict'
 
-/////// Задача 1
+///// Задача 1
 
-// let a;
-// let b;
-// let c;
+let a;
+let b;
+let c;
 
-// function getSolutions( a, b, c) {
+function getSolutions( a, b, c) {
 
-//     let D = ( b**2 - 4 * a * c);
+    let D = ( b**2 - 4 * a * c);
 
-//     if (D < 0) {
-//         return {D: D};
-//     } else if (D === 0) {
-//         let x1 = (- b / (2 * a));
-//         return { 
-//             roots: x1,
-//             D: D
-//         }
-//     } else if (D > 0) {
-//         let x1 = (( -b + Math.sqrt(D) ) / ( 2 * a ) );
-//         let x2 = (( -b - Math.sqrt(D) ) / ( 2 * a ) );
-//         return {
-//             roots: [x1, x2],
-//             D: D    
-//         }
-//     }  
-// }
+    if (D < 0) {
+        return {D: D};
+    } else if (D === 0) {
+        let x1 = (- b / (2 * a));
+        return { 
+            roots: x1,
+            D: D
+        }
+    } else if (D > 0) {
+        let x1 = (( -b + Math.sqrt(D) ) / ( 2 * a ) );
+        let x2 = (( -b - Math.sqrt(D) ) / ( 2 * a ) );
+        return {
+            roots: [x1, x2],
+            D: D    
+        }
+    }  
+}
 
-// function showSolutionsMessage( a, b, c) {
+function showSolutionsMessage( a, b, c) {
 
-//     let result = getSolutions(a,b,c);
+    let result = getSolutions(a,b,c);
 
-//     console.log(`Вычисляем корни квадратного уравнения ${a}*x^2 + ${b}*x + ${c} = 0`);
-//     console.log(`Значение дискриминанта: ${result.D}`);
+    console.log(`Вычисляем корни квадратного уравнения ${a}*x^2 + ${b}*x + ${c} = 0`);
+    console.log(`Значение дискриминанта: ${result.D}`);
 
-//     if (result.D < 0) {
-//         console.log('Уравнение не имеет вещественных корней');
-//     } else if (!result.D) {
-//         console.log(`Уравнение имеет один корень X₁ = ${result.roots}`);
-//     } else if (result.D) {
-//         console.log(`Уравнение имеет два корня. X₁ = ${ result.roots[0] }, X₂ = ${ result.roots[1] }`)   
-//     }  
-// };
+    if (result.D < 0) {
+        console.log('Уравнение не имеет вещественных корней');
+    } else if (!result.D) {
+        console.log(`Уравнение имеет один корень X₁ = ${result.roots}`);
+    } else if (result.D) {
+        console.log(`Уравнение имеет два корня. X₁ = ${ result.roots[0] }, X₂ = ${ result.roots[1] }`)   
+    }  
+};
 
-// showSolutionsMessage(1,2,3);
-// showSolutionsMessage(7,20,-3);
-// showSolutionsMessage(2,4,2);
+showSolutionsMessage(1,2,3);
+showSolutionsMessage(7,20,-3);
+showSolutionsMessage(2,4,2);
 
-// ///////// Задача 2
+///////// Задача 2
 
-// let data = {
-//     algebra: [2, 5, 3, 6, 2, 6],
-//     geometry: [5, 4, 3, 7, 2],
-//     russian: [5, 3, 5, 2],
-//     physics: [6, 3, 5, 2],
-//     music: [ 4, 5 ,5, 5],
-//     english: [5, 2, 5, 4, 5 ],
-//     poetry: [3, 4, 5, 2, 2 ],
-//     chemistry: [4, 5 ],
-//     french: [ 4, 3, 5 ],
+let data = {
+    algebra: [2, 5, 3, 6, 2, 6],
+    geometry: [5, 4, 3, 7, 2],
+    russian: [5, 3, 5, 2],
+    physics: [6, 3, 5, 2],
+    music: [ 4, 5 ,5, 5],
+    english: [5, 2, 5, 4, 5 ],
+    poetry: [3, 4, 5, 2, 2 ],
+    chemistry: [4, 5 ],
+    french: [ 4, 3, 5 ],
   
-// };
+};
 
-// function getAverageScore (arr, newArr) {
+function getAverageScore (arr, newArr) {
 
-//     function getAverageScoreOfLesson(arr) {   
-//         let sumOfMarks = 0;        
-//         for (let i = 0; i < arr.length; i++) {
-//             sumOfMarks += arr[i];
-//         };
-//         return sumOfMarks / arr.length;
-//     }
+    function getAverageScoreOfLesson(arr) {   
+        let sumOfMarks = 0;        
+        for (let i = 0; i < arr.length; i++) {
+            sumOfMarks += arr[i];
+        };
+        return sumOfMarks / arr.length;
+    }
     
-//     for (let prop in arr) {
-//         let value = arr [ prop ];
+    for (let prop in arr) {
+        let value = arr [ prop ];
 
-//         newArr = arr ;
-//         newArr[prop] = getAverageScoreOfLesson(value);
-//     }
+        newArr = arr ;
+        newArr[prop] = getAverageScoreOfLesson(value);
+    }
 
-//     let j = 0;
-//     let sum = 0;
+    let j = 0;
+    let sum = 0;
 
-//     for (let prop in newArr) {
-//         sum +=  newArr [ prop ];
-//         j++;
-//     }
+    for (let prop in newArr) {
+        sum +=  newArr [ prop ];
+        j++;
+    }
 
-//     newArr.average = ( sum / j );
+    newArr.average = ( sum / j );
 
-//     console.log(newArr);
-// };
+    console.log(newArr);
+};
 
-// getAverageScore (data);
+getAverageScore (data);
 
-//////// Задача 3
+////// Задача 3
 
 let secretData = {
     aaa: 0,
