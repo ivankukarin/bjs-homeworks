7094e04b57be12dd7e56309fe356b54bf1dc366a
ivankukarin <ikukarin@yandex.ru>
2019-07-02 22:47:15 +0400

    less 4 task1, f calcMonth is good

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,10 +12,24 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    
+    let P = percent/100/12;
+    console.log(P);
+    const calcMonths = () => 
+       Math.floor((((new Date(date)).getTime()) - (new Date().getTime()))/( 86400000 * 29.4 )); /// принимаю 29.4 дня в месяце усредненное 
+    
+    let n = calcMonths();
+    console.log(n);
+    let monthlyPayment = amount * ( P + P /((Math.pow (( 1 + P ), n )-1)));
+    console.log(monthlyPayment);
+    let totalAmount = monthlyPayment * n;
+    
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    return totalAmount;
 }
+
+
+//////////// Задача 2
 
 function sayHello() {
     let name = window.personName.value;
