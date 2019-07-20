94dd0ab4abd9104e4562fa6db5475e4984e0bcac
ivankukarin <ikukarin@yandex.ru>
2019-07-20 10:38:17 +0400

    in progress...

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,15 +1,15 @@
 "use strict";
 
-// Array.isArray(); ///
-// name.indexOf(value); /// вернет порядковый номер (и -1 если его нет в массиве)
-// arr.includes(item); /// true/false
-// name.lastIndexOf(value); /// с конца массива
-// arr.findIndex(funct); // возвращает индекс
-// arr.find(funct); // возвращаем сам элемент  (numbers.find(isEven)===undefined)
-// numbers.some(func); // возвращает true если бы хотя бы один соответствует func
-// arr.every(func); // true если только все соответствуют func
-// empoloyees.filter(func); //  вернет новый массив с соответствующим функции
-// arr.map(func); // вернет новый массив с новыми элементами каждый из оторых прееобразован с помощью func
+Array.isArray(); ///
+name.indexOf(value); /// вернет порядковый номер (и -1 если его нет в массиве)
+arr.includes(item); /// true/false
+name.lastIndexOf(value); /// с конца массива
+arr.findIndex(funct); // возвращает индекс
+arr.find(funct); // возвращаем сам элемент  (numbers.find(isEven)===undefined)
+numbers.some(func); // возвращает true если бы хотя бы один соответствует func
+arr.every(func); // true если только все соответствуют func
+empoloyees.filter(func); //  вернет новый массив с соответствующим функции
+arr.map(func); // вернет новый массив с новыми элементами каждый из оторых прееобразован с помощью func
 
 // let compare = (arr1, arr2) => {
 //   (arr1.length === arr2.length)
@@ -27,8 +27,24 @@ const compareArrays = function(arr1, arr2) {
   );
 };
 
-compareArrays([8, 9], [6]); // false, разные значения
-compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
-compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
-compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
-compareArrays([8, 1, 2], [8, 1, 2]); // true
+const sum = (a, b) => a + b;
+const mSum = memoize(sum, 2);
+
+sum(3, 4); // 7
+mSum(3, 4); // 7
+
+function memoize(fn, limit) {
+  const memory = [];
+  return function() {
+    let args = Array.from(arguments);
+    let checkAc = memory.find(mem => compareArrays(mem, args));
+    if (checkAc === undefined) {
+      console.log(`Функция вызвана не из памяти.`);
+      return checkAc.result;
+    } else {
+      memory.push({ arg: args, result: fn.apply(args) });
+
+      return fn.apply(args);
+    }
+  };
+}
