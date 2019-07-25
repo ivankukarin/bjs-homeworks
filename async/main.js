2f9c53c8a768da570151ce0377afcae48e326b77
ivankukarin <ikukarin@yandex.ru>
2019-07-25 13:45:47 +0400

    corrected

--- async/main.js
+++ async/main.js
@@ -1,36 +1,36 @@
 "use strict";
 
-function setDailyRhythm(wakeUpTime, bedTime) {
-  const setAlarm = function(time, callback) {
-    let timeNow = new Date();
-    const doTwoNumber = x => {
-      if (x < 10) {
-        x = "0" + x;
-      }
-      return x;
-    };
-
-    let hours = timeNow.getHours();
-    let minutes = timeNow.getMinutes();
-    const formatHHmm = t => {
-      let hh = doTwoNumber(hours);
-      let mm = doTwoNumber(minutes);
-      return `${hh}:${mm}`;
-    };
-
-    const fc = () => {
-      if (formatHHmm(timeNow) === time) {
-        callback();
-      }
-    };
-
-    return fc;
+const setAlarm = function(time, callback) {
+  let timeNow = new Date();
+  const doTwoNumber = x => {
+    if (x < 10) {
+      x = "0" + x;
+    }
+    return x;
   };
 
+  let hours = timeNow.getHours();
+  let minutes = timeNow.getMinutes();
+  const formatHHmm = t => {
+    let hh = doTwoNumber(hours);
+    let mm = doTwoNumber(minutes);
+    return `${hh}:${mm}`;
+  };
+
+  const func = (time, callback) => {
+    if (formatHHmm(timeNow) === time) {
+      callback();
+    }
+  };
+
+  return func(time, callback);
+};
+
+function setDailyRhythm(wakeUpTime, bedTime) {
   const goodMorning = () => alert("Доброе утро, Вася!");
   const goodNight = () => alert("Спокойной ночи, Вася!");
   setInterval(setAlarm(wakeUpTime, goodMorning), 1000);
   setInterval(setAlarm(bedTime, goodNight), 1000);
 }
 
-setDailyRhythm("13:47", "13:49");
+setDailyRhythm("13:43", "13:44");
